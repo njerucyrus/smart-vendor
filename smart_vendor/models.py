3ff4b8e793fda515a2fc2e415461e60d00e438c2
Njeru Cyrus <njerucyrus5@hotmail.com>
2024-03-30 21:45:49 +0300

    added alembic

--- smart_vendor/models.py
+++ smart_vendor/models.py
@@ -24,20 +24,23 @@ class UserAccount(Base):
     user_id = Column(String, ForeignKey("users.id"))
     available_balance = Column(Numeric(precision=9, scale=2))
     user = relationship('User', back_populates='accounts')
+    top_ups = relationship('TopUp', back_populates='account')
     location = Column(String, nullable=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow())
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow())
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
 
 
 class TopUp(Base):
     __tablename__ = 'topups'
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()).replace('-', ''))
+    account_id = Column(String, ForeignKey("user_accounts.id"))
+    account = relationship('UserAccount', back_populates='top_ups')
     txn_id = Column(String, unique=True)
     receipt_no = Column(String, nullable=True)
     phone_number = Column(String(20), nullable=True)
     amount = Column(Numeric(precision=9, scale=2))
     status = Column(Enum('pending', 'success', 'failed', name='status_enum'))
-    date = Column(DateTime, default=datetime.datetime.utcnow())
+    date = Column(DateTime, default=datetime.datetime.utcnow)
 
 
 
